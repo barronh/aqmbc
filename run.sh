4422d94fd8538f7430c3800344a529b581fcf913
barronh <barronh@gmail.com>
2020-05-07 16:30:55 -0400

    Cleaning up run

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 # GRID for output files
 # see GRIDDESC for options
-export GRID=12US1
+export GRID=TEST
 
 # Whitespace Delimited list of definition files
 # to use in translating input mechanism to output mechanism
@@ -12,11 +12,11 @@ export DEFN="${PWD}/definitions/gc/gc_airmolden.expr ${PWD}/definitions/gc/gc11_
 
 # Link files
 DATAROOT=/work/ROMO/global/GCv11-01/GC/rundirs/geosfp_2x25_soa/ND49/
-ln -fs ${DATAROOT}/ts.newfinn.htap.nei11mon.newvolc.20160101.bpch CONC/
-ln -fs ${DATAROOT}/*info.dat BCON/
-ln -fs /work/ROMO/global/CMAQv5.2.1/2016fe_hemi_cb6_16jh/108km/basecase/output/CONC/CCTM_CONC_*_20160101
-# Run all or bcon or combine or fig
+# DATAROOT=/work/ROMO/global/CMAQv5.2.1/2016fe_hemi_cb6_16jh/108km/basecase/output/CONC/
 export FILEPAT='ts.*'
-export FILEPAT='CCTM_*'
+# export FILEPAT='CCTM_*'
+ln -fs ${DATAROOT}/${FILEPAT} CONC/
+ln -fs ${DATAROOT}/*info.dat BCON/
+# Run all or bcon or combine or fig
 make all
 
